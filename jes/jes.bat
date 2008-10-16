67a75d399175bfdd0441cfff4062edeb1ff1e6f1
bgbuck7 <bgbuck7@56e44fc4-9ba9-11dd-a29e-e3fa11317253>
2008-10-16 18:44:10 +0000

    New jes.bat launcher to detect if java exists

--- jes/jes.bat
+++ jes/jes.bat
@@ -1,6 +1,12 @@
-set java="win-jre\bin\java.exe"
 set classpath=".\jars\jython.jar;.\jars\jmf.jar;.\jars\jl1.0.jar;.\jars\junit.jar;.\Sources"
+java -version:1.5+ -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
 
 
-%java% -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
+IF NOT %ERRORLEVEL%==0 (
+	set java="win-jre\bin\java.exe"
 
+
+	%java% -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
+
+
+)

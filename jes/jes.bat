a7212618d76aa4266ecc6637f8159480eb41751a
bgbuck7 <bgbuck7@56e44fc4-9ba9-11dd-a29e-e3fa11317253>
2008-10-22 05:47:57 +0000

    Fixed jes.bat

--- jes/jes.bat
+++ jes/jes.bat
@@ -2,6 +2,5 @@ set classpath=".\jars\jython.jar;.\jars\jmf.jar;.\jars\jl1.0.jar;.\jars\junit.ja
 java -version:1.6+ -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
 
 IF %ERRORLEVEL%==1 (
-	set java="win-jre\bin\java.exe"
-	%java% -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
+	"win-jre\bin\java.exe" -Dpython.home="jython-2.2.1" -Xmx512m -classpath %classpath% JESstartup
 )

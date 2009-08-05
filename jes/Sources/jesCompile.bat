4f37a313ff27157e25068adeb5997d6ce1cd6cb7
bjdorn <bjdorn@56e44fc4-9ba9-11dd-a29e-e3fa11317253>
2009-08-05 17:39:29 +0000

    Changing compilation and run scripts so that we target java 1.5 jre

--- jes/Sources/jesCompile.bat
+++ jes/Sources/jesCompile.bat
@@ -1,4 +1,4 @@
 set classpath=".;..\jars\jython.jar;..\jars\jmf.jar;..\jars\jl1.0.jar;..\jars\junit.jar;..\jars\customizer.jar;..\jars\mediaplayer.jar;..\jars\multiplayer.jar"
 
-javac -target 1.6 *.java -cp %classpath% 
+javac -target 1.5 *.java -cp %classpath% 
 javadoc *.java -d javadoc
